d7301895f9b49a0cc947ae2caed2d518b45e5598
Jason Evans <jason.evans@rvu.co.uk>
2025-07-03 13:54:17 +0100

    Initial explaination of my current settings

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("core.lsp")
+
+vim.cmd("colorscheme kanagawa")
+
+vim.cmd("let g:kanagawa_transparent_bg = 1")
+
+vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
