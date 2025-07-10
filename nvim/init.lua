91f960072410162470de78d2899348672c6f96cc
Jason Evans <jason.evans@rvu.co.uk>
2025-07-10 15:35:59 +0100

    Latest changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,4 +6,7 @@ vim.cmd("colorscheme kanagawa")
 
 vim.cmd("let g:kanagawa_transparent_bg = 1")
 
+--vim.cmd("colorscheme nordfox")
+--vim.cmd("let g:nordfox_transparent_bg = 1")
+
 vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
