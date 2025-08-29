f75b941f1105a4d9786bd0d814746c979cb8e8ec
Jason Evans <jason.evans@rvu.co.uk>
2025-08-29 11:36:12 +0100

    Add DAP support for debugging TypeScript

---
+++ nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+	  "nvim-neotest/nvim-nio"
+    },
+    config = function()
+      -- Here you can add your dap configurations
+      -- for example, setting up the pwa-node adapter
+    end,
+  },
+}
