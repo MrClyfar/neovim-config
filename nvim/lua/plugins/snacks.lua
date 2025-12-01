74afe0e8a523ebf66d9ef38bdb4c0e7d33d0d890
Jason Evans <jason.evans@rvu.co.uk>
2025-12-01 07:23:39 +0000

    plugin(snacks): Config the border and placement of windows for file search

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -59,12 +59,12 @@ return {
             border = "top",
             title = " {title} {live} {flags}",
             title_pos = "left",
-            { win = "input", height = 1, border = "bottom" },
             {
               box = "horizontal",
               { win = "list", border = "none" },
               { win = "preview", title = "{preview}", width = 0.6, border = "left" },
             },
+            { win = "input", height = 1, border = "top" },
           },
         },
       },
