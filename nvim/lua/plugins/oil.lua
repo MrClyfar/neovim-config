7fb517c638edbbff2ca11df5a4139923614af6ab
Jason Evans <jason.evans@rvu.co.uk>
2025-12-01 07:23:13 +0000

    plugin(oil): Config to show current folder
    Display the current folder path in the oil UI.

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,7 +1,23 @@
 -- https://github.com/stevearc/oil.nvim
+
+-- Custom function for displaying the current directory in the winbar
+function _G.get_oil_winbar()
+  local dir = require("oil").get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~") -- ":~" simplifies home dir to "~"
+  else
+    -- If there is no dir, just return the current buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
   "stevearc/oil.nvim",
   opts = {
+    win_options = {
+      -- This sets the winbar to call the function we defined above
+      winbar = "%!v:lua.get_oil_winbar()",
+    },
     view_options = {
       -- Show files and directories that start with "."
       show_hidden = true,
