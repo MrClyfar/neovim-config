e6bdf127d632c0cc7c2658be2e37b3d10b1b5b4f
Jason Evans <jason.evans@rvu.co.uk>
2025-12-01 07:23:57 +0000

    plugin: Add yukinord theme

---
+++ nvim/lua/plugins/yukinord.lua
@@ -0,0 +1,8 @@
+-- https://github.com/adibhanna/yukinord.nvim
+return {
+  "adibhanna/yukinord.nvim",
+  config = function()
+    -- vim.cmd([[colorscheme yukinord]])
+    -- vim.cmd("let g:yukinord_transparent_bg = 1")
+  end,
+}
